c395c187fecc2b89b3effe48f9d972b6d4bf3f58
Luke Dowling <lpdowling26@googlemail.com>
2024-07-18 11:52:17 +0200

    added extra information to characters and updated styles

--- src/components/Character.tsx
+++ src/components/Character.tsx
@@ -1,22 +1,27 @@
 import {
   Card,
   CardBody,
+  CardFooter,
   CardHeader,
   GridItem,
   Heading,
   Image,
+  Text,
 } from "@chakra-ui/react";
 
 interface Character {
   name: string | undefined;
   image: string | undefined;
+  species: string | undefined;
+  status: string | undefined;
+  type: string | undefined;
 }
 
-const Character = ({ name, image }: Character) => {
+const Character = ({ name, image, species, status, type }: Character) => {
   return (
-    <GridItem>
-      <Card align={"center"} variant={"outline"} h={280} m={2}>
-        <CardHeader p={2}>
+    <GridItem h={"max-content"}>
+      <Card align={"center"} variant={"outline"} m={2}>
+        <CardHeader p={2} h={50}>
           <Heading size={"md"} textAlign={"center"}>
             {name}
           </Heading>
@@ -30,6 +35,24 @@ const Character = ({ name, image }: Character) => {
             objectFit={"cover"}
           />
         </CardBody>
+        <CardFooter
+          flexDirection={"column"}
+          padding={2}
+          alignSelf={"flex-start"}
+          h={95}
+        >
+          <Text>Species: {species}</Text>
+          <Text>Status: {status}</Text>
+          {type ? (
+            <Text
+              whiteSpace={"nowrap"}
+              overflow={"hidden"}
+              textOverflow={"ellipsis"}
+            >
+              Type: {type}
+            </Text>
+          ) : null}
+        </CardFooter>
       </Card>
     </GridItem>
   );
