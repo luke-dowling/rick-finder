be76cfdffc9c001137d707a48a6aecd60788b589
Luke Dowling <lpdowling26@googlemail.com>
2024-07-18 10:09:03 +0200

    reordered the imports in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
 import { AppRoutes } from "./Routes.tsx";
 import { AppProvider } from "./context/AppContext.tsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <ChakraProvider>
