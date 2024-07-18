7b2ce902128579863b8eb1519e3dbc6ffddbe3b0
Luke Dowling <lpdowling26@googlemail.com>
2024-07-18 10:07:24 +0200

    added container for app

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,15 +2,16 @@ import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Search } from "./pages/Search";
 import Header from "./components/Header";
+import { Box } from "@chakra-ui/react";
 
 export const AppRoutes = () => {
   return (
-    <>
+    <Box mx={[4, 10, 20, "auto"]} maxW={980}>
       <Header />
       <Routes>
         <Route index element={<Home />} />
         <Route path="search" element={<Search />} />
       </Routes>
-    </>
+    </Box>
   );
 };
