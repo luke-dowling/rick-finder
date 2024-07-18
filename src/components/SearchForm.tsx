455c5c0a9ae06d83ab66e5341803e258a109eafe
Luke Dowling <lpdowling26@googlemail.com>
2024-07-18 11:51:43 +0200

    added focus to input

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,8 +1,7 @@
-import { FormLabel, Input, Button } from "@chakra-ui/react";
-import { FormEvent, useRef } from "react";
+import { Input, Button } from "@chakra-ui/react";
+import { FormEvent, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAppContext } from "../context/AppContext";
-const baseUrl = "https://rickandmortyapi.com/api/character/";
 
 export const SearchForm = () => {
   const { updateSearchResults, setLoading, search, updateSearch, setError } =
@@ -12,20 +11,33 @@ export const SearchForm = () => {
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
+    if (searchInputRef.current!.value === "") return;
     setLoading(true);
     setError("");
 
     updateSearch({ name: searchInputRef.current!.value });
-    updateSearchResults(`${baseUrl}?name=${searchInputRef.current!.value}`);
+    updateSearchResults(`?name=${searchInputRef.current!.value}`);
     setLoading(false);
     navigate("/search");
   };
 
+  useEffect(() => {
+    searchInputRef.current!.focus();
+  });
+
   return (
     <form onSubmit={handleSubmit}>
-      <FormLabel>Search a character from Rick & Morty:</FormLabel>
-      <Input type="text" ref={searchInputRef} defaultValue={search.name} />
-      <Button type="submit">Search</Button>
+      <Input
+        borderRadius={20}
+        my={[2, 5]}
+        type="text"
+        ref={searchInputRef}
+        defaultValue={search.name}
+        placeholder="Search a character from Rick & Morty..."
+      />
+      <Button type="submit" display={"none"}>
+        Search
+      </Button>
     </form>
   );
 };
